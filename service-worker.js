e9f8243e85b3e8399a86c27a5dab2677ef1bda59
Mainetti-Solution <mainetti.solution@gmail.com>
2025-05-04 18:27:58 +0200

    Primo caricamento

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'bollettario-cache-v1';
+const urlsToCache = ['/', '/index.html', '/style.css', '/script.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
